630930bf77b229e21aa01c8a4d7dd4380eeeeeb3
Cosmanderio <maxime.noe2009@yahoo.com>
2026-02-10 18:22:37 +0100

    Création automatique de l'environnement virtuel pour python

--- install.bat
+++ install.bat
@@ -1,5 +1,17 @@
 @echo off
-REM Installation des paquets requis
-pip install -r requirements.txt
 
+if exist .venv\ (
+    echo Virtual environment already existing.
+) else (
+    echo Creation of a virtual environment...
+    python -m venv .venv
+)
+
+REM Installation des paquets requis
+.venv\Scripts\pip.exe install -r requirements.txt
+
+echo:
 echo Installation completed successfully.
+echo:
+echo To run the program : .venv\Scripts\python.exe sources\main.py
+echo:
